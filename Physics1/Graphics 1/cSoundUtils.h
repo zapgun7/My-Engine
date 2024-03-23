6ad3eae84a46c58e336e867bc417c823a341a022
zapgun7 <68291699+zapgun7@users.noreply.github.com>
2024-03-23 19:37:36 -0400

    Delete Physics1/Graphics 1/cSoundUtils.h

--- Physics1/Graphics 1/cSoundUtils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <fmod/fmod.hpp>
-
-const char* GetFMODErrorString(FMOD_RESULT result);
-void CheckFMODError(FMOD_RESULT result, const char* file, int line);
-#define FMOD_ERROR_CHECK(result) CheckFMODError(result, __FILE__, __LINE__)
